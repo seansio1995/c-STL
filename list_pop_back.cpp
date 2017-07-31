15acf1e4d8cd3c4cb3a8e9547da9a65c621f634c
seansio1995 <cx7ga@virginia.edu>
2017-07-31 12:23:05 +0800

    updates

---
+++ list_pop_back.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main(){
+
+  list<int> mylist;
+
+  int sum=0;
+
+  mylist.push_back(100);
+  mylist.push_back(200);
+
+  mylist.push_back(300);
+
+  while(!mylist.empty()){
+    sum+=mylist.back();
+    mylist.pop_back();
+
+  }
+
+  cout<<"The total sum is "<<sum<<endl;
+
+  return 0;
+}
